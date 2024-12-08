056024413788fb6317ed50dc1199951b390f8091
letYuchan <yuchancho174@naver.com>
2024-12-08 22:24:14 +0900

    front mod: modify color(all page) intermediate work

--- digitalCard-date-service/front_part/src/idealTypeSurvey/selectedButton.js
+++ digitalCard-date-service/front_part/src/idealTypeSurvey/selectedButton.js
@@ -1,10 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // 선택 가능한 모든 요소 가져오기
-    const selectableDivs = document.querySelectorAll(".survey_section-container div[id]");
+    // MBTI 그룹의 모든 div 가져오기
+    const mbtiGroups = document.querySelectorAll(".mbti-group");
+  
+    mbtiGroups.forEach((group) => {
+      const mbtiOptions = group.querySelectorAll("div[id]");
+  
+      mbtiOptions.forEach((option) => {
+        option.addEventListener("click", () => {
+          // 해당 그룹에서 이미 선택된 요소 해제
+          mbtiOptions.forEach((opt) => opt.classList.remove("selected"));
+  
+          // 현재 클릭한 요소 선택
+          option.classList.add("selected");
+        });
+      });
+    });
+  
+    // 나머지 요소 처리 (선택 제한 없음)
+    const selectableDivs = document.querySelectorAll(".survey_section-container div[id]:not(.mbti-group div[id])");
   
     selectableDivs.forEach((div) => {
       div.addEventListener("click", () => {
-        // 선택된 상태를 토글 (추가/제거)
         div.classList.toggle("selected");
       });
     });
